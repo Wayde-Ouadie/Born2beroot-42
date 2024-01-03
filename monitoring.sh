a7476427aa38584806ad5a5ba0448d160654836b
Ouadie El Fengour <oel-feng@e1r4p14.1337.ma>
2024-01-03 01:06:40 +0100

    B2BR

---
+++ monitoring.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+wall "	----------------------------------------------------------
+	#Architecture	: $(uname -a)
+	#Physical CPUs	: $(grep -c 'physical id' /proc/cpuinfo)
+	#Virtual CPUs	: $(grep -c processor /proc/cpuinfo)
+	#Memory Usage	: $(free --mega | grep Mem | awk '{print $3}')/$(free --mega | grep Mem | awk '{print $2}')MB ($(free -k | grep Mem | awk '{printf("%.2f%%"), $3/$2*100}'))
+	#Disk Usage	: $(df -BM | grep '^/dev/' | awk '{ut += $3} END {print ut}')/$(df -BG | grep '^/dev/' | awk '{tt += $2} END {print tt}')Gb ($(df -BM | grep '^/dev/' | awk '{ut += $3} {tt += $2} END {printf("%.2f%%", ut/tt*100)}'))
+	#CPU Load	: $(iostat | grep -A 1 idle | grep '^ ' | awk '{printf("%.2f%%\n", 100 - $NF)}')
+	#Last Boot	: $(who -b | awk '{printf $3 " " $4}')
+	#LVM Use	: $(if [ $(lsblk | grep -c "lvm") -eq 0 ]; then echo no; else echo yes; fi)
+	#Connections TCP: $(netstat -t | grep -c "ESTABLISHED") ESTABLISHED
+	#User log	:
+	#Network	:
+	#Sudo		:
+	----------------------------------------------------------"
