7f9aeba02d1bd27fc42e47aacdb247f00e24f97f
Ouadie El Fengour <ouadie.xyz1@gmail.com>
2024-01-07 05:06:24 +0100

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -4,12 +4,12 @@ wall "	----------------------------------------------------------
 	#Physical CPUs	: $(grep -c 'physical id' /proc/cpuinfo)
 	#Virtual CPUs	: $(grep -c processor /proc/cpuinfo)
 	#Memory Usage	: $(free --mega | grep Mem | awk '{print $3}')/$(free --mega | grep Mem | awk '{print $2}')MB ($(free -k | grep Mem | awk '{printf("%.2f%%"), $3/$2*100}'))
-	#Disk Usage	: $(df -BM | grep '^/dev/' | awk '{ut += $3} END {print ut}')/$(df -BG | grep '^/dev/' | awk '{tt += $2} END {print tt}')Gb ($(df -BM | grep '^/dev/' | awk '{ut += $3} {tt += $2} END {printf("%.2f%%", ut/tt*100)}'))
+	#Disk Usage	: $(df -BM | grep '^/dev/' | awk '{ut += $3} END {print ut}')/$(df --total -h | grep 'total' | awk '{print $2}')b ($(df --total -h | grep 'total' | awk '{print $5}'))
 	#CPU Load	: $(iostat | grep -A 1 idle | grep '^ ' | awk '{printf("%.2f%%\n", 100 - $NF)}')
 	#Last Boot	: $(who -b | awk '{printf $3 " " $4}')
 	#LVM Use	: $(if [ $(lsblk | grep -c "lvm") -eq 0 ]; then echo no; else echo yes; fi)
 	#Connections TCP: $(netstat -t | grep -c "ESTABLISHED") ESTABLISHED
 	#User log	: $(who -u | awk '{print $1}' | uniq | wc -l)
-	#Network	: IP $(hostname -I) ($(cat /sys/class/net/enp0s3/adress))
+	#Network	: IP $(hostname -I)($(cat /sys/class/net/enp0s3/address))
 	#Sudo		: $(journalctl -q _COMM=sudo | grep -c "COMMAND")
 	----------------------------------------------------------"
